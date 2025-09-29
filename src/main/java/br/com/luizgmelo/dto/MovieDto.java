7e51541357300df984711a04fa47318b05b3adc9
Luiz Guilherme <luizgmelo64@gmail.com>
2025-09-29 15:22:14 -0300

    refac: movieDto

--- src/main/java/br/com/luizgmelo/dto/MovieDto.java
+++ src/main/java/br/com/luizgmelo/dto/MovieDto.java
@@ -1,13 +1,23 @@
 package br.com.luizgmelo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MovieDto {
+
     private String title;
+
     private String overview;
+
+    @JsonProperty("release_date")
     private String releaseDate;
-    private Long popularity;
+
+    private Double popularity;
 
     public String getTitle() {
         return title;
@@ -33,11 +43,36 @@ public class MovieDto {
         this.releaseDate = releaseDate;
     }
 
-    public Long getPopularity() {
+    public String getReleaseDateFormatted() {
+        if (releaseDate == null || releaseDate.isEmpty()) {
+            return "";
+        }
+
+        try {
+            DateTimeFormatter formatInput = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter formatOutput = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            LocalDate date = LocalDate.parse(releaseDate, formatInput);
+            return date.format(formatOutput);
+        } catch (DateTimeParseException e) {
+            System.err.println("Erro ao formatar data: " + releaseDate);
+            return releaseDate;
+        }
+    }
+
+    public Double getPopularity() {
         return popularity;
     }
 
-    public void setPopularity(Long popularity) {
+    @Override
+    public String toString() {
+        return "\n" + "Título: " + title + "\n" +
+                "Descrição: " + overview + "\n" +
+                "Data de Lançamento: " + getReleaseDateFormatted() + "\n" +
+                "Popularidade: " + String.format("%.2f", popularity);
+    }
+
+    public void setPopularity(Double popularity) {
         this.popularity = popularity;
     }
 }
